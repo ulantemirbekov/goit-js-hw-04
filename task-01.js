e9f15e519ed433c026a90294c02840a797399636
Ulan Temirbekov <temirbekovulanbek@gmail.com>
2020-11-20 10:18:09 +0200

    Update task-01.js

--- task-01.js
+++ task-01.js
@@ -17,3 +17,12 @@ const account = {
         this.orders.push(order);
     },
 };
+
+account.changeDiscount(0.15);
+console.log(account.discount); // 0.15
+
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3']
+
+account.addOrder(5000, 'order-4');
+console.log(account.balance); // 19000
+console.table(account.showOrders()); // ['order-1', 'order-2', 'order-3', 'order-4']
